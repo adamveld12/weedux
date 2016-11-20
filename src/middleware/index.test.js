e27a0fc51864f4369b5b3776f81c46cf9674d5af
Adam Veldhousen <adamveld12@gmail.com>
2016-11-20 11:06:25 -0600

    added test to try out passing middleware without array literal

--- src/middleware/index.test.js
+++ src/middleware/index.test.js
@@ -1,8 +1,30 @@
 import Weedux from '../index.js';
-import { async } from  './index.js';
+import { async, logger } from  './index.js';
 
-test('async dispatch 1', () => {
+test('async middleware applies when not passed as array', () => {
   const store = new Weedux({}, (s) => s, async);
+  const dispatch = store.dispatcher();
+  const testFn = jest.fn();
+
+  store.onDispatchComplete(ns => testFn());
+
+  return new Promise((resolve) => {
+
+    dispatch((d) => {
+      setTimeout(() => {
+        d({});
+        resolve();
+      });
+
+      dispatch({});
+      expect(testFn).toHaveBeenCalledTimes(1);
+    });
+
+  }).then(() => expect(testFn).toHaveBeenCalledTimes(2));
+});
+
+test('async middleware applies when passed as array', () => {
+  const store = new Weedux({}, (s) => s, [async]);
   const dispatch = store.dispatcher();
   const testFn = jest.fn();
 
