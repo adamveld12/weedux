f2bea16539822834b92165c55fe6244fdb4bc7d7
Adam Veldhousen <adamveld12@gmail.com>
2017-06-06 21:07:12 -0500

    some comments for the middlewares

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,11 +1,21 @@
+/*
+Allows passing a function to a dispatch call that receivees a dispatcher and the current state as arguments.
+This allows you to execute asynchronous calls and dispatch as they are executed
+*/
 const createThunk = store => n => a =>
   (typeof(a) === "function") ? a(store.dispatcher(), store.getState()) : n(a)
 
+/*
+Logs actions before they are dispatched
+*/
 const createLogger = (store) => n => a => {
   console.log("Dispatching ", a);
   n(a);
 }
 
+/*
+Allows you to handle errors globally that are thrown while reducing and action
+*/
 const createSafeDispatcher = (store, onError) => (n) => {
   try{
     n(a);
